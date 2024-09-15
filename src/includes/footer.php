97be94f1158f79ac5e2bd512c71afb60033f1b5c
Vitor Lopes <48527153+vitorglopes@users.noreply.github.com>
2024-09-14 22:06:06 -0300

    fix: front links

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -7,7 +7,7 @@ namespace src\views\includes;
     <p class="roboto-bold">&copy; The Boring Blog, <?= date("Y") ?> - Todos os direitos reservados.</p>
 </footer>
 <script type="text/javascript" src="<?= SITE_PUBLIC ?>js/libs/jquery-3.7.1.min.js"></script>
-<script type="text/javascript" src="<?= SITE_PUBLIC ?>js/libs/bootstrap-5.3.3.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script type="text/javascript" src="<?= SITE_PUBLIC ?>js/libs/bootstrap-5.3.3.bundle.min.js"></script>
 <script type="text/javascript" src="<?= SITE_PUBLIC ?>js/libs/select2.full.min.js"></script>
 <script type="text/javascript">
     const SITE_ADDRESS = "<?= SITE_ADDRESS ?>";
--- src/views/404.php
+++ src/views/404.php
@@ -10,6 +10,14 @@ namespace src\views;
 <head>
     <title>404</title>
     <meta charset="utf-8" lang="pt-br">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="<?= SITE_PUBLIC ?>css/bootstrap-5.3.3.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH">
+    <link href="<?= SITE_PUBLIC ?>css/roboto.css" rel="stylesheet">
+    <link href="<?= SITE_PUBLIC ?>css/select2.min.css" rel="stylesheet">
+    <link href="<?= SITE_PUBLIC ?>css/principal.css" rel="stylesheet">
+    <link href="<?= SITE_PUBLIC ?>css/icons.css" rel="stylesheet">
     <style>
         a {
             text-decoration: none;
@@ -21,9 +29,9 @@ namespace src\views;
 <body>
     <div style="text-align: center;">
         <img width="500" height="300" src="<?= SITE_PUBLIC ?>img/404-not-found.svg" alt="Página não encontrada">
-        <h2>Página não encontrada</h2>
+        <p class="roboto-bold">Página não encontrada</p>
         <br>
-        <a href="<?= SITE_ADDRESS . "home/index" ?>">Voltar para o início</a>
+        <a class="roboto-thin-bold" href="<?= SITE_ADDRESS . "home/index" ?>">Voltar para o início</a>
     </div>
 </body>
 
